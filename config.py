3a826d4baebd46b79102ce466a60991b9438990f
Carlos Chinchilla <calufa@gmail.com>
2019-10-10 16:53:09 -0500

    add config

---
+++ config.py
@@ -0,0 +1,22 @@
+# Warning: the position of these variables is sensitive
+ASSETS_DIR_PATH = '/assets'
+TMP_DIR_PATH = '{}/tmp'.format(ASSETS_DIR_PATH)
+LIBRARY_DIR_PATH = '/library'
+# End of sensitive variables
+FINAL_VIDEO_FILE_PATH = '{}/video.mov'.format(TMP_DIR_PATH)
+FPS = 30
+GEN_TRANSPARENT_FILE_PATH = '{}/transparent.mov'.format(TMP_DIR_PATH)
+IMAGE_DURATION = 4
+IMAGES_TRANSITIONS_FILE_PATH = '{}/images-transitions.mov'.format(TMP_DIR_PATH)
+IMAGES_VIDEO_FILE_PATH = '{}/images.mov'.format(TMP_DIR_PATH)
+PIXEL_FMT = 'argb'
+STORY_FILE_PATH = 'story.yaml'
+TRANSITION_FILE_PATH = 'transitions.yaml'
+TRANSITION_ID = 0
+TRANSITIONS_FILE_PATH = '{}/transitions.mov'.format(TMP_DIR_PATH)
+TRANSITIONS_LIBRARY_DIR_PATH = '{}/transitions'.format(LIBRARY_DIR_PATH)
+TRANSPARENT_FILE_PATH = '{}/transparent-30fps-512x1024.mov'.format(LIBRARY_DIR_PATH)
+VIDEO_CODEC = 'qtrle'
+VIDEO_FILE_PATH = '{}/video.mp4'.format(ASSETS_DIR_PATH)
+VIDEO_FMT = 'mov'
+VIDEO_SIZE = (512, 1024)
