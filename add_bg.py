895245f9f340db84421edd688f0a183fd7290f2e
Carlos Chinchilla <calufa@gmail.com>
2019-10-15 22:34:51 -0500

    adjust add_bg.py

--- add_bg.py
+++ add_bg.py
@@ -1,22 +1,17 @@
 import subprocess
 
-from add_images import (add_audio_to_video,
-						generate_silence_audio,
-						overlay_videos)
-from config import *
-
-
 def crop_story_video():
 	cmd = 'ffmpeg \
 		-y \
 		-i {input_file_path} \
-		-vf "crop={width}:{height}:0:0" \
+		-vf "crop={width}:{height}:{padding}:{padding}" \
 		-c:v {video_codec} \
 		-pix_fmt {pixel_fmt} \
 		{output_file_path}'.format(
 			input_file_path=STORY_VIDEO_WITH_PRESENTER_BG_FILE_PATH,
 			width=VIDEO_SIZE[0] - (BACKGROUND_PADDING * 2),
 			height=VIDEO_SIZE[1] - (BACKGROUND_PADDING * 2),
+			padding=BACKGROUND_PADDING,
 			video_codec=VIDEO_CODEC,
 			pixel_fmt=PIXEL_FMT,
 			output_file_path=CROPPED_STORY_VIDEO_FILE_PATH)
@@ -56,13 +51,15 @@ def pad_story_video():
 if __name__ == '__main__':
 	crop_story_video()
 	pad_story_video()
-
+	# Add audio to background
 	duration = get_duration(PADDED_STORY_VIDEO_FILE_PATH)
-	generate_silence_audio(duration, SILENCE_AUDIO_FILE_PATH)
+	generate_silence_audio(duration)
+	tmp_file_path = get_tmp_file_path(BACKGROUND_WITH_AUDIO_FILE_PATH)
 	add_audio_to_video(SILENCE_AUDIO_FILE_PATH,
 					   BACKGROUND_VIDEO_FILE_PATH,
-					   BACKGROUND_WITH_AUDIO_FILE_PATH)
-
+					   tmp_file_path)
+	cut_video(tmp_file_path, BACKGROUND_WITH_AUDIO_FILE_PATH, duration)
+	# Merge background with story
 	video_file_paths = (
 		BACKGROUND_WITH_AUDIO_FILE_PATH,
 		PADDED_STORY_VIDEO_FILE_PATH
