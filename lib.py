afbf8c4f781fd61b8f8d670c391b4f85df114540
Carlos Chinchilla <calufa@gmail.com>
2019-10-15 22:01:07 -0500

    add cut_video()

---
+++ lib.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+def cut_video(input_file_path, output_file_path, duration):
+	cmd = 'ffmpeg \
+		-y \
+		-i {input_file_path} \
+		-ss 0 \
+		-to {duration} \
+		-c:v {video_codec} \
+		-pix_fmt {pixel_fmt} \
+		{output_file_path}'.format(
+			input_file_path=input_file_path,
+			duration=duration,
+			video_codec=VIDEO_CODEC,
+			pixel_fmt=PIXEL_FMT,
+			output_file_path=output_file_path)
+	subprocess.call(['bash', '-c', cmd])
