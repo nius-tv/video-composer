5f4c1c0a926ba08e5d57540cce51061358fe7408
Carlos Chinchilla <calufa@gmail.com>
2019-10-25 02:46:40 -0500

    add category video step

---
+++ add_category_logo.py
@@ -0,0 +1,15 @@
+from config import *
+from utils import *
+
+
+if __name__ == '__main__':
+	duration = get_duration(CATEGORY_VIDEO_FILE_PATH)
+	generate_silence_audio(duration)
+	add_audio_to_video(SILENCE_AUDIO_FILE_PATH,
+					   CATEGORY_VIDEO_FILE_PATH,
+					   CATEGORY_WITH_AUDIO_VIDEO_FILE_PATH)
+	video_file_paths = (
+		COMPOSED_VIDEO_FILE_PATH,
+		CATEGORY_WITH_AUDIO_VIDEO_FILE_PATH
+	)
+	overlay_videos(video_file_paths, FINAL_VIDEO_FILE_PATH)
