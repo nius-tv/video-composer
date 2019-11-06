4bc8c75ec4a71eab467980a36a4960abea2d48b1
Carlos Chinchilla <calufa@gmail.com>
2019-11-06 15:32:02 -0500

    enhance calc_transition logic

--- calc_transitions.py
+++ calc_transitions.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 from config import *
 from utils import *
@@ -10,19 +11,24 @@ if __name__ == '__main__':
 	oov_frames = story['outOfVocabularyFrames']
 	oov_frames_keys = sorted(oov_frames)
 
-	from_sec = float(oov_frames_keys[0]) / FPS
-	last_key = oov_frames_keys[-1]
-	to_sec = float(oov_frames[last_key]) / FPS
+	if len(oov_frames_keys) > 1:
+		from_sec = float(oov_frames_keys[0]) / FPS
+		last_key = oov_frames_keys[-1]
+		to_sec = float(oov_frames[last_key]) / FPS
 
-	start = 0
-	if from_sec > TRANSITIONS_MIN_START:
-		start = from_sec
+		start = 0
+		if from_sec > TRANSITIONS_MIN_START:
+			start = from_sec
 
-	end = duration
-	if to_sec < duration - TRANSITIONS_MIN_END:
-		end = to_sec
+		end = duration
+		if to_sec < duration - TRANSITIONS_MIN_END:
+			end = to_sec
 
-	num_images = math.ceil((end - start) / float(IMAGE_DURATION))
+		num_images = math.ceil((end - start) / float(IMAGE_DURATION))
+	else:
+		start = random.randrange(TRANSITIONS_MIN_START)
+		max_num_images = math.floor((duration - TRANSITIONS_MIN_START - TRANSITIONS_MIN_END) / float(IMAGE_DURATION))
+		num_images = random.randrange(1, max_num_images)
 
 	story['transitions'] = {
 		'start': start,
