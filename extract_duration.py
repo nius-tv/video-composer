d18d1a3a4f5a56fdb09294674b2e2a8b24db9280
Carlos Chinchilla <calufa@gmail.com>
2019-11-07 16:26:17 -0500

    use STORY_VIDEO_WITH_CATEGORY_FILE_PATH

--- extract_duration.py
+++ extract_duration.py
@@ -4,5 +4,5 @@ from utils import *
 
 if __name__ == '__main__':
 	data = load_story()
-	data['duration'] = get_duration(STORY_VIDEO_FILE_PATH)
+	data['duration'] = get_duration(STORY_VIDEO_WITH_CATEGORY_FILE_PATH)
 	save_story(data)
