1d5294a93c7b09b8174b14e614eec85d65192edc
Carlos Chinchilla <calufa@gmail.com>
2019-12-04 01:52:39 -0500

    use STORY_VIDEO_NO_AUDIO_FILE_PATH

--- add_mask.py
+++ add_mask.py
@@ -3,13 +3,13 @@ from utils import *
 
 
 if __name__ == '__main__':
-	duration = get_duration(STORY_VIDEO_FILE_PATH)
+	duration = get_duration(STORY_VIDEO_NO_AUDIO_FILE_PATH)
 	# Cut mask to match story duration
 	tmp_mask_file_path = get_tmp_file_path(MASK_FILE_PATH)
 	cut_video(MASK_FILE_PATH, tmp_mask_file_path, duration)
 
 	video_file_paths = (
-		STORY_VIDEO_FILE_PATH,
+		STORY_VIDEO_NO_AUDIO_FILE_PATH,
 		tmp_mask_file_path
 	)
 	overlay_videos(video_file_paths, STORY_VIDEO_WITH_MASK_FILE_PATH)
