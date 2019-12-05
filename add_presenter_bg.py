93f044bda1d3b1644c155008980cf8f4b4f6a1cf
Carlos Chinchilla <calufa@gmail.com>
2019-12-05 01:02:44 -0500

    add duration to add_presenter_bg

--- add_presenter_bg.py
+++ add_presenter_bg.py
@@ -1,9 +1,11 @@
 import subprocess
 
 from config import *
+from utils import *
 
 
 if __name__ == '__main__':
+	duration = get_duration(STORY_VIDEO_FILE_PATH)
 	cmd = 'ffmpeg \
 		-y \
 		-i {video_file_path} \
@@ -13,15 +15,17 @@ if __name__ == '__main__':
 			[1:v][v0]overlay[v1]" \
 		-map [v1] \
 		-map 0:a \
+		-ss 0 \
+		-to {duration} \
 		-c:a {audio_codec} \
 		-c:v {video_codec} \
 		-pix_fmt {pixel_fmt} \
-		-shortest \
 		{output_file_path}'.format(
 			video_file_path=STORY_VIDEO_FILE_PATH,
 			background_file_path=PRESENTER_BACKGROUND_VIDEO_FILE_PATH,
 			chroma=CHROMA_SENSITIVITY,
 			overlay=CHROMA_OVERLAY,
+			duration=duration,
 			audio_codec=AUDIO_CODEC,
 			video_codec=VIDEO_CODEC,
 			pixel_fmt=PIXEL_FMT,
