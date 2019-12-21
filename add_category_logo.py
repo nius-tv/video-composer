6cc9e00b335828992e80f2d821e21914e6f6296d
Carlos Chinchilla <calufa@gmail.com>
2019-12-20 19:27:43 -0500

    abstract category

--- add_category_logo.py
+++ add_category_logo.py
@@ -3,10 +3,12 @@ from utils import *
 
 
 if __name__ == '__main__':
-	duration = get_duration(CATEGORY_VIDEO_FILE_PATH)
+	category = load_story()['category']
+	category_video_file_path = '{}/{}.{}'.format(CATEGORY_VIDEO_DIR_PATH, category, VIDEO_FMT)
+	duration = get_duration(category_video_file_path)
 	generate_silence_audio(duration)
 	add_audio_to_video(SILENCE_AUDIO_FILE_PATH,
-					   CATEGORY_VIDEO_FILE_PATH,
+					   category_video_file_path,
 					   CATEGORY_WITH_AUDIO_VIDEO_FILE_PATH)
 	video_file_paths = (
 		COMPOSED_VIDEO_FILE_PATH,
